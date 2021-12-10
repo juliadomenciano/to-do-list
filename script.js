382373d4e495ec8db2bbbe7de3226b5a0333e30e
Maju Domenciano <juliadomenciano@hotmail.com>
2021-12-10 20:47:09 -0300

    Requisitos obrigatorios

--- script.js
+++ script.js
@@ -3,37 +3,56 @@ let criarTarefa = document.getElementById('criar-tarefa')
 let listaTarefas = document.getElementById('lista-tarefas')
 let apagaTudo = document.getElementById('apaga-tudo')
 let RemoveF = document.getElementById('remover-finalizados')
+/* let li = document.querySelectorAll('.list-item') */
 
 criarTarefa.addEventListener('click', addTarefa)
-/* listaTarefas.addEventListener('click', select) */
+listaTarefas.addEventListener('click', select)
 listaTarefas.addEventListener('dblclick', cross)
 apagaTudo.addEventListener('click', removeList)
 RemoveF.addEventListener('click', removeCrossed)
-
+input.addEventListener('keyup', function(event){
+    if(event.keyCode === 13){
+        criarTarefa.click()
+    }
+})
 
 function addTarefa(){
     let li = document.createElement('li')
     li.classList.add('list-item')
-    li.classList.add('selected')
     li.innerHTML = input.value
     listaTarefas.appendChild(li)
     input.value = ''
 }
 
-/* let listItem = document.querySelector('list-item') */
+
+
 
 /* function select(event){
-    let item = document.querySelectorAll('li')
-    for(let i in item){
 
-    }
-   
-    else{
-        event.target.classList.toggle('.selected')
-        event.target.style.backgroundColor = 'rgb(128, 128, 128)'
-    }
-    
+    let selected = document.querySelector('.selected')
+    console.log(selected)
+    selected.classList.remove('selected')
+    event.target.classList.add('selected')
 } */
+
+function select(event){
+    if(event.target.classList !== 'selected'){
+            let sib = event.target.parentElement.children
+        for(let i of sib){
+            i.classList.remove('selected')
+    }
+        event.target.classList.add('selected')
+    }
+    else{
+        
+        event.target.classList.toggle('selected')
+    
+    }
+}
+
+
+
+
 
 function cross(event){
     event.target.classList.toggle('completed')  
@@ -41,15 +60,15 @@ function cross(event){
 
 function removeList(){
     let item = document.querySelectorAll('.list-item')
-    for(let i in item){
-        item[i].remove()
+    for(let i of item){
+        i.remove('.list-item')
     }
 }
 
 function removeCrossed(){
     let item = document.querySelectorAll('.completed')
     console.log(item)
-    for(let i in item){
-        item[i].remove()
+    for(let i of item){
+        i.remove('completed')
     }
 }
