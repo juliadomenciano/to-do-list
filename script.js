d526816350e1ed65a8535cbf808cfb109a8a436a
Maju Domenciano <juliadomenciano@hotmail.com>
2021-12-13 20:46:05 -0300

    requisito 12

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let criarTarefa = document.getElementById('criar-tarefa')
 let listaTarefas = document.getElementById('lista-tarefas')
 let apagaTudo = document.getElementById('apaga-tudo')
 let RemoveF = document.getElementById('remover-finalizados')
-/* let li = document.querySelectorAll('.list-item') */
+let saveList = document.getElementById('salvar-tarefas')
 
 criarTarefa.addEventListener('click', addTarefa)
 listaTarefas.addEventListener('click', select)
 listaTarefas.addEventListener('dblclick', cross)
 apagaTudo.addEventListener('click', removeList)
 RemoveF.addEventListener('click', removeCrossed)
+saveList.addEventListener('click', save)
+
 input.addEventListener('keyup', function(event){
     if(event.keyCode === 13){
         criarTarefa.click()
@@ -22,18 +24,9 @@ function addTarefa(){
     li.innerHTML = input.value
     listaTarefas.appendChild(li)
     input.value = ''
+    
 }
-
-
-
-
-/* function select(event){
-
-    let selected = document.querySelector('.selected')
-    console.log(selected)
-    selected.classList.remove('selected')
-    event.target.classList.add('selected')
-} */
+ 
 
 function select(event){
     if(event.target.classList !== 'selected'){
@@ -49,10 +42,6 @@ function select(event){
     
     }
 }
-
-
-
-
 
 function cross(event){
     event.target.classList.toggle('completed')  
@@ -70,5 +59,37 @@ function removeCrossed(){
     console.log(item)
     for(let i of item){
         i.remove('completed')
+    }
+}
+
+function save(){
+    let items = document.querySelectorAll('.list-item')
+    let listObj = []
+    
+    for(let i = 0; i < items.length; i++){
+        let obj = {'item': items[i].innerHTML, 'class':items[i].classList}
+        listObj.push(obj)
+        
+    }
+    localStorage.setItem('list', JSON.stringify(listObj))
+
+
+    
+}
+window.onload = function(){
+    for(let i = 0; i < localStorage.length; i+=2){
+        let list = JSON.parse(localStorage.getItem('list'))
+        for(let i = 0; i < list.length;i++){
+            let item = document.createElement('li')
+            item.innerHTML = list[i].item
+            let itemClass =  list[i].class
+            for(let c = 0; c < itemClass[0].length;c++){
+                let classL = itemClass[c]
+                console.log(classL)
+                item.classList.add(classL)
+            }
+/*             console.log(list[i].item) */
+            listaTarefas.appendChild(item)
+        }
     }
 }
